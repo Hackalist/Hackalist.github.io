f934f6da87b3ed67347986e1638630097d6471fe
Kevin Payravi <kevinpayravi@gmail.com>
2014-09-01 18:35:09 -0400

    Revert

--- js/app.js
+++ js/app.js
@@ -1,14 +1,7 @@
 var app = angular.module('hackalist', []);
 
 app.controller('hackathonEvents', ['$http', '$scope', function($http, $scope){
-  
-  $scope.keys = function(obj){
-    return obj? Object.keys(obj) : [];
-  }
-
-  $scope.Dates = {
-    $http.get('../api/1.0/api.json').success(function (obj) {
-      $scope.year = obj;
-    }
+  $http.get('../api/1.0/api.json').success(function (data) {
+    $scope.year = data;
   })
 }]);
