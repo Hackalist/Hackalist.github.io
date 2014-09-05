db92528b52e0792c6f8e3fb09961b27fc2f8634d
Kevin Payravi <kevinpayravi@gmail.com>
2014-09-05 01:42:12 -0400

    Testing new API structure

--- js/app.js
+++ js/app.js
@@ -1,7 +1,10 @@
 var app = angular.module('hackalist', []);
 
 app.controller('hackathonEvents', ['$http', '$scope', function($http, $scope){
-  $http.get('../api/1.0/api.json').success(function (data) {
-    $scope.year = data;
+  $http.get('../api/1.0/august14.json').success(function (data) {
+    $scope.august14 = data;
+  })
+  $http.get('../api/1.0/september14.json').success(function (data) {
+    $scope.september14 = data;
   })
 }]);
