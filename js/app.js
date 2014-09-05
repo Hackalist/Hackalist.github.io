449a06428513816e407fa565a382b410a5ab92e3
Kevin Payravi <kevinpayravi@gmail.com>
2014-09-05 19:09:07 -0400

    API folder restructure

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
 var app = angular.module('hackalist', []);
 
 app.controller('hackathonEvents', ['$http', '$scope', function($http, $scope){
-  $http.get('../api/1.0/august14.json').success(function (data) {
+  $http.get('../api/1.0/2014/08.json').success(function (data) {
     $scope.august14 = data;
   })
-  $http.get('../api/1.0/september14.json').success(function (data) {
+  $http.get('../api/1.0/2014/09.json').success(function (data) {
     $scope.september14 = data;
   })
-  $http.get('../api/1.0/october14.json').success(function (data) {
+  $http.get('../api/1.0/2014/10.json').success(function (data) {
     $scope.october14 = data;
   })
-  $http.get('../api/1.0/november14.json').success(function (data) {
+  $http.get('../api/1.0/2014/11.json').success(function (data) {
     $scope.november14 = data;
   })
-  $http.get('../api/1.0/december14.json').success(function (data) {
+  $http.get('../api/1.0/2014/12.json').success(function (data) {
     $scope.december14 = data;
   })
-  $http.get('../api/1.0/january15.json').success(function (data) {
+  $http.get('../api/1.0/2015/01.json').success(function (data) {
     $scope.january15 = data;
   })
-  $http.get('../api/1.0/february15.json').success(function (data) {
+  $http.get('../api/1.0/2015/02.json').success(function (data) {
     $scope.february15 = data;
   })
-  $http.get('../api/1.0/march15.json').success(function (data) {
+  $http.get('../api/1.0/2015/03.json').success(function (data) {
     $scope.march15 = data;
   })
-  $http.get('../api/1.0/april15.json').success(function (data) {
+  $http.get('../api/1.0/2015/04.json').success(function (data) {
     $scope.april15 = data;
   })
 }]);
